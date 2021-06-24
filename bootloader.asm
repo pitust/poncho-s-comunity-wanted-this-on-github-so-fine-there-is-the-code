f09515dc13c3a7343415d8f8e9f06d4c435b93db
pitust <piotr@stelmaszek.com>
2021-06-24 19:24:24 +0100

    Fixed some disk IO related bugs, and cleaned up the loading code a bit. Also added a gitignore.
    Signed-Off-By: pitust <piotr@stelmaszek.com>

--- bootloader.asm
+++ bootloader.asm
@@ -1,18 +1,45 @@
 
 [org 0x7c00]
 
-mov [BOOT_DISK], dl
+    ; note(pitust): you may not need it here tbh
+    mov [BOOT_DISK], dl
 
-mov bp, 0x7c00
-mov sp, bp
+    mov ah, 0x42
+    mov si, boot_dap
+    int 0x13
+    jc diskreadFailed
 
-call ReadDisk
+    mov bp, 0x7c00
+    mov sp, bp
 
-jmp PROGRAM_SPACE
+    jmp 0x7e00
+
 
 %include "print.asm"
-%include "diskread.asm"
 
+BOOT_DISK:
+    db 0
+
+diskreadErrorString:
+    db "disk read failed",0
+
+diskreadFailed:
+    mov bx, diskreadErrorString
+    call PrintString
+    
+    jmp $
+
+boot_dap:
+    db 0x10 ; dap size
+    db 0x00 ; unused
+    dw (bin_end - bin_start + 511) / 512 ; number of sectors
+    dw 0x7e00 ; offset of destination
+    dw 0x0000 ; segment of destination
+    dq 1 ; start from second sector
 times 510-($-$$) db 0
 
+
 dw 0xaa55
+bin_start:
+incbin "kernel.bin"
+bin_end:
